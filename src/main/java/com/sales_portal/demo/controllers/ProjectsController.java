37498d5ca92636b73f375091d8deecf0adb25d48
AlexandraCristetiu <alexandra.cristetiu@gmail.com>
2019-10-14 17:00:40 +0300

    Functional Search in allCompanies

---
+++ src/main/java/com/sales_portal/demo/controllers/ProjectsController.java
@@ -0,0 +1,40 @@
+package com.sales_portal.demo.controllers;
+
+import com.sales_portal.demo.services.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
+@Controller
+@AllArgsConstructor
+public class ProjectsController {
+    private ProjectService projectService;
+
+
+    @RequestMapping({"mvc/projects/showAll"})
+    public ModelAndView showAllProjects(){
+        ModelAndView mv = new ModelAndView("showProjectList");
+        mv.addObject("project",projectService.getAllProjects());
+        return mv;
+    }
+
+    @GetMapping("mvc/project/insert")
+    public String insertProject(){
+        return "insertProject";
+    }
+
+    @PostMapping("mvc/project/insert")
+    public ModelAndView insertProject(String company_name, String PO_number, Double amount,
+                                      String project_status, Date start_date, Date delivery_date, String contact_person){
+       projectService.insertProject(company_name,PO_number,amount,project_status,start_date,delivery_date,contact_person);
+        return showAllProjects();
+
+    }
+
+}
